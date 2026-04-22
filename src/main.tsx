d1fbf88338281adb0bd0f6af4112c9e81b8651ef
zuni4064 <zunair4064@gmail.com>
2026-04-22 21:03:23 +0500

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// main.tsx
+import { HelmetProvider } from 'react-helmet-async';
 
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
+);
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
